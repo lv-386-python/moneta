4a3a8f3f9b448e4e25aebfd19f7d22f815d1b3ee
MaksymGromyk <maksymgrom@gmail.com>
2019-03-27 12:39:20 +0200

    Added database schema diagram and minor changes  to structure

--- moneta/db/moneta/schema/user.sql
+++ moneta/db/moneta/schema/user.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS db_moneta.user (
   password VARCHAR(45) NOT NULL,
   def_currency ENUM("UAH", "USD", "EUR", "GBP") NOT NULL,
   email VARCHAR(45) NOT NULL,
+  is_activated TINYINT NOT NULL,
   PRIMARY KEY (id)
 )
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
