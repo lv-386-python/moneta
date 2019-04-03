4075ba16c68de18de581c549c3eb5564ae21c74c
jelysaw <jelysaw@gmail.com>
2019-04-03 13:48:45 +0300

    Edit templates

--- moneta/src/python/authentication/forgot_password.py
+++ moneta/src/python/authentication/forgot_password.py
@@ -1,45 +1,48 @@
-"""forgot password views and functions"""
+"""Forgot password views and functions."""
 import random
 import string
 from django.shortcuts import render
 from django.core.mail import send_mail
-from helper import pool_manager as db
+
+try:
+    from helper import pool_manager as db
+except ImportError:
+    pass
 
 def have_email_from_user(request):
-    """Get user email"""
+    """Get user email."""
     if request.method == "POST":
         try:
             user_email = request.POST.get('email')
         except ValueError:
-            pass
-        return user_email
+            return None
+    return user_email
 
 def random_string(stringlength=10):
-    """Generate a random string of fixed length"""
+    """Generate a random string of fixed length."""
     password = string.ascii_lowercase
     return ''.join(random.choice(password) for i in range(stringlength))
 
 @db.re_request()
-def find_user_by_email(our_user):
-    """Find user in database by his email"""
+def find_user_by_email(user_email):
+    """Find user in database by his email."""
     try:
-        query = f"Select id, email from user where email = '{our_user}'"
+        query = f"Select id, email from user where email = '{user_email}'"
         with db.pool_manage().manage() as connect:
             cursor = connect.cursor()
             cursor.execute(query)
-            sql_str = cursor.fetchall()  # make a list from sets
+            sql_str = cursor.fetchall()
     except ValueError:
         return None
     if not sql_str:
         return None
-    else:
-        return our_user
+    return user_email
 
 @db.re_request()
-def save_password_in_db(our_user, new_password):
-    """Update user password"""
+def save_password_in_db(user_email, new_password):
+    """Update user password."""
     try:
-        changed_password = f"UPDATE user SET password = '{new_password}' WHERE email = '{our_user}'"
+        changed_password = f"UPDATE user SET password = '{new_password}' WHERE email = '{user_email}'"
         with db.pool_manage().manage() as connect:
             cursor = connect.cursor()
             cursor.execute(changed_password)
@@ -48,7 +51,7 @@ def save_password_in_db(our_user, new_password):
     return new_password
 
 def user_not_exist(request):
-    """Check if user does not exist"""
+    """Check if user does not exist."""
     if request.method == "POST":
         try:
             email_user = have_email_from_user(request)
@@ -57,10 +60,11 @@ def user_not_exist(request):
             return None
         if not have_sql:
             have_sql = None
-        return have_sql
+    return have_sql
+
 
 def send_email(new_password, user_email):
-    """Send a messsage to user"""
+    """Send a messsage to user."""
     try:
         send_mail('no reply',
                   f'HELLO from "Moneta"! Your new password is  {new_password}',
@@ -70,7 +74,7 @@ def send_email(new_password, user_email):
         pass
 
 def reset_user_password(request):
-    """View to reset user password"""
+    """View to reset user password."""
     if request.method == "POST":
         try:
             user_response = user_not_exist(request)
@@ -79,8 +83,7 @@ def reset_user_password(request):
                 password = save_password_in_db(user_response, new_user_password)
                 send_email(password, user_response)
                 return render(request, "authentication/valid_email.html")
-            else:
-                return render(request, "authentication/not_user.html")
+            return render(request, "authentication/not_user.html")
         except ValueError:
             return render(request, "authentication/not_user.html")
     return render(request, "authentication/forgot_password.html")
