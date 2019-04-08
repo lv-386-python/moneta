f916f441e091e551a919920bad4185a9228e18e0
jelysaw <jelysaw@gmail.com>
2019-04-08 10:14:14 +0300

    Change object name

--- moneta/src/python/db/reset_password.py
+++ moneta/src/python/db/reset_password.py
@@ -10,14 +10,11 @@ class ResetPassword():
     @decorators.retry_request()
     def find_user_by_email(user_email):
         """Find user in database by his email."""
-        try:
-            query = f"Select id, email from auth_user where email = '{user_email}'"
-            with db.DBPoolManager().get_connect() as connect:
-                cursor = connect.cursor()
-                cursor.execute(query)
-                sql_str = cursor.fetchall()
-        except ValueError:
-            return None
+        query = f"Select id, email from auth_user where email = '{user_email}'"
+        with db.DBPoolManager().get_connect() as connect:
+            cursor = connect.cursor()
+            cursor.execute(query)
+            sql_str = cursor.fetchall()
         if not sql_str:
             return None
         return user_email
@@ -26,29 +23,26 @@ class ResetPassword():
     @decorators.retry_request()
     def save_password_in_db(user_email, new_password):
         """Update user password."""
-        try:
-            changed_password = f"UPDATE auth_user SET password = '{new_password}'" \
-                f" WHERE email = '{user_email}'"
-            with db.DBPoolManager().get_connect() as connect:
-                cursor = connect.cursor()
-                cursor.execute(changed_password)
-        except ValueError:
-            return None
+        changed_password = f"UPDATE auth_user SET password = '{new_password}'" \
+            f" WHERE email = '{user_email}'"
+        with db.DBPoolManager().get_connect() as connect:
+            cursor = connect.cursor()
+            cursor.execute(changed_password)
         return new_password
 
 def user_exists(request):
     """Check if user does not exist."""
     if request.method == "POST":
         try:
-            user_object = ResetPassword()
+            user = ResetPassword()
             user_email = forgot_password.have_email_from_user(request)
-            have_sql = user_object.find_user_by_email(user_email)
+            have_sql = user.find_user_by_email(user_email)
         except ValueError:
             return None
         if not have_sql:
             have_sql = None
         else:
             new_user_password = utils.random_string()
-            password = user_object.save_password_in_db(user_email, new_user_password)
+            password = user.save_password_in_db(user_email, new_user_password)
             utils.send_email(password, user_email)
     return have_sql
