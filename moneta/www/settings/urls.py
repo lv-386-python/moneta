304f71d6be13d86737428c4f6748b115c395acae
jelysaw <jelysaw@gmail.com>
2019-04-22 11:12:41 +0300

    Refactor

--- moneta/www/settings/urls.py
+++ moneta/www/settings/urls.py
@@ -47,9 +47,7 @@ urlpatterns = [
     path('current/<int:current_id>/edit/', current_views.current_edit, name='current_edit'),
     # ex: /current/5/delete/
     path('current/<int:current_id>/delete/', current_views.current_delete, name='current_delete'),
-
     path('income/', income_views.income_list, name='edit_income'),
-
     path('income/<int:income_id>/edit/', income_views.edit_income, name='edit_income'),
     path('income/<int:income_id>/delete/', income_views.delete_income, name='edit_income')
 
