ad22d418f86c085eca0e1c34e6bbc6e4e7b98944
VolodymyrBodnar <volodymyrbodnar.97@gmail.com>
2019-05-09 15:43:33 +0300

    create form builder function

--- moneta/www/static/js/form.js
+++ moneta/www/static/js/form.js
@@ -1,4 +1,51 @@
-// Get the modal
+
+function buildForm(data){
+    // build html form as string
+    // Args:
+    //  data(json): data about new form
+    // Returns:
+    //  formHTML (string) : html of form.
+
+    let formHTML = `<div><form class="basic_form" id="${data.form_id}"> `;
+    formHTML += `<h1>${data.form_name}</h1>`;
+    for(const inputField of data.fields){
+        let newInput = '<div class="form-group">';
+        newInput += `<label>${inputField.label}</label>`;
+        newInput += `<input class="form-control" type="${inputField.type}" value="${inputField.value}">`;
+        newInput += '</div>';
+        formHTML += newInput;
+    }
+    formHTML += '<button type="submit" class="btn btn-primary">Submit</button>';
+    formHTML += ' </form></div>';
+    return formHTML
+}
+
+
+// When the user clicks the button, open the modal
+$(document).on('click', '#addExpend', function (e) {
+    // data = {
+    //     'form_id':'test_form_id',
+    //     'form_name':'Test form',
+    //     'fields':[
+    //         {'label':'test_label 1', 'type':'text','value':'lorem'},
+    //         {'label':'test_label 2', 'type':'text','value':'ipsum'},
+    //         {'label':'test_label 3', 'type':'text','value':'dolor'},
+    //         {'label':'test_label 4', 'type':'text','value':'sit'},
+    //         {'label':'test_label 5', 'type':'text','value':'amen'},
+    //     ]
+    // }
+    $.get("expend/create", function (data) {       
+        console.log(data);
+        newForm = buildForm(data);
+        $("#modalF").html(newForm);
+        $('#incomeForm').css("display", "flex");
+    });
+
+})
+
+
+
+
 
 // When the user clicks the button, open the modal
 $(document).on('click', '#addIncome', function (e) {
@@ -9,6 +56,7 @@ $(document).on('click', '#addIncome', function (e) {
     });
 
 })
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -81,5 +129,14 @@ $(document).on('click', '#userSettingsForm', function (event) {
     if (event.target.id === "userSettingsForm") {
         $("#userSettingsForm").css("display", "none");
         $("#userSettingsForm").children().empty();
+    }
+});
+
+// Close popup if user press ESC button
+$(document).keydown(function(e){
+    if ( e.keyCode === 27 ) {
+        $(incomeForm).css("display","none");
+        $(currentForm).css("display","none");
+        $(expendForm).css("display","none");
     }
 });
--- moneta/www/views/expend.py
+++ moneta/www/views/expend.py
@@ -8,7 +8,7 @@ import json
 
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
