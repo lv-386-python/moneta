a5e43e1de03b239760fd9f126b4e7cc69d57434d
zaiets <irynazaiets98@ukr.net>
2019-05-14 10:28:07 +0300

    Adequate design for log-in form

--- moneta/www/views/login_view.py
+++ moneta/www/views/login_view.py
@@ -41,8 +41,7 @@ def login_view(request):
     if form.is_valid():
         user = authenticate(email=form['email'].value(), password=form['password'].value())
         if not user:
-            print("not user")
-            return render(request, "login_app/login.html", {'form': form, "err": 'UDE'})
+            return render(request, "login_app/login.html", {'form': form, "err": 'Email does not exist'})
         if Registration.is_active(user.id):
             login(request, user)
             if 'next' in request.POST:
