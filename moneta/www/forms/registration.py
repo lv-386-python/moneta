127cd51da469cf203d9add77c7d3fbbf080c8f43
shado1997 <shado20120@gmail.com>
2019-05-15 14:32:48 +0300

    added style for registration

--- moneta/www/forms/registration.py
+++ moneta/www/forms/registration.py
@@ -3,13 +3,12 @@ from django import forms
 
 from src.python.db.currencies import Currency
 
-Cur = [{'', 'Select default currency'}]
+Cur = [{'', 'Default currency'}]
 List = Cur + Currency.currency_list()
 
 class SignUpForm(forms.Form):
     """ Class for creating form for registration.  """
     email = forms.EmailField(max_length=45, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    print(List)
     select_default_currency = forms.ChoiceField(
         widget=forms.Select(attrs={'class': 'select_cur'}),
         choices=List,
