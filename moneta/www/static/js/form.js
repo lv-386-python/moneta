9bccc0664d0d7dbf985301c953a35fa6d70f35de
Volodymyr Bodnar <vbodnar72@gmail.com>
2019-05-20 07:53:55 +0000

    Revert "Hotfix/transaction ui"

--- moneta/www/static/js/form.js
+++ moneta/www/static/js/form.js
@@ -108,6 +108,12 @@ $(document).on('click', '#cancel_form', function(e){
     $('.bg-modal').css("display","none");
 });
 
+$(document).on('click', '#cancel_one_level_up_form',  function (event) {
+    $.get(window.location.href.split('/').slice(0, 3).join('/') + '/user_settings/', function (data) {
+        $(".modal-content").html(data);
+    });
+});
+
 $(document).on('click', '.icon_option', function (e) {
     $(CHOSED_ICON).toggleClass('icon_selected');
     $(e.target).toggleClass('icon_selected');
@@ -196,12 +202,13 @@ $(document).on('click','#editIncome', function (e){
 });
 
 // CURRENT
+// When the user clicks the button, open the modal
 $(document).on('click', '#addCurrent', function (e) {
     let info = {
         'method':'POST',
-        'api_url':'api/v1/current/create'
+        'api_url':'/api/v1/current/create'
     };
-    getInfoAndBuildForm('Create Income',info); 
+    getInfoAndBuildForm('Create Current',info);
 });
 
 $(document).on('click','#editCurrent', function (e){
@@ -219,48 +226,10 @@ $(document).on('click','#editCurrent', function (e){
 
 ///When the user press button "user profile" open user profile page
 $(document).on('click', '#userSettings', function (e) {
-    $.get('user_settings/', function (data) {
+    $.get( window.location.href.split('/').slice(0, 3).join('/') + '/user_settings/', function (data) {
         $('.modal-content').html(data);
         $('.bg-modal').css("display", 'flex');
 
-    });
-});
-
-///Close user profile when user click somewhere except form
-$(document).on('click', '#userSettingsForm', function (event) {
-    if (event.target.id === "userSettingsForm") {
-        $(".bg-modal").css("display", "none");
-        $(".modal-content").children().empty();
-    }
-});
-
-$(document).on('click', '#goBack',  function (event) {
-    $.get("user_settings/", function (data) {
-        $(".modal-content").html(data);
-    });
-});
-
-$(document).on('click', '#goBack1',  function (event) {
-    $.get("user_settings/", function (data) {
-        $(".bg-modal").css("display", "none");
-    });
-});
-
-$(document).on('click', '#goBack2',  function (event) {
-    $.get("user_settings/", function (data) {
-        $(".modal-content").html(data);
-    });
-});
-
-$(document).on('click', '#goBack3',  function (event) {
-    $.get("user_settings/", function (data) {
-        $(".modal-content").html(data);
-    });
-});
-
-$(document).on('click', '#goBack4',  function (event) {
-    $.get("/", function (data) {
-        $(".bg-modal").css("display", "none");
     });
 });
 
